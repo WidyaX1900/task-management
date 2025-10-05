5db02c422bae55908b4a8495e85bebfe18a490df
WidyaX1900 <tuderangga93@gmail,com>
2025-10-05 12:39:59 +0800

    refactor: put the modal element inside a component

---
+++ resources/views/components/my-modal.blade.php
@@ -0,0 +1,7 @@
+@props(['id'])
+
+<div id="{{ $id }}" class="fixed top-0 left-0 w-full h-full px-8 z-50 bg-black/85 flex justify-center items-center">
+    <div class="bg-gray-900 p-3 w-full rounded border-2 border-gray-600 drop-shadow">
+        {{ $slot }}
+    </div>
+</div>
